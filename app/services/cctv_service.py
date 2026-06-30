420f89810b0c508afeb208d457662ad8b61d763d
320-06\ji0 <wldud9130@gamil.com>
2026-06-30 14:16:40 +0900

    fix : cctv 키밸류 수정

--- app/services/cctv_service.py
+++ app/services/cctv_service.py
@@ -32,11 +32,11 @@ MONITORED_CCTV_LOCATIONS = [
         "searchRegions": [{"lat": 37.2816, "lon": 127.2507, "delta": 0.05}],
     },
     {
-        'id': 'godeok_tunnel',
-        'display_name': '[세종포천선] 고덕터널',
-        'location_name': '고덕터널',
-        'search_names': ['[세종] 고덕터널', '고덕터널', '세종 2'],
-        'search_regions': [{'lat': 37.5436, 'lon': 127.1685, 'delta': 0.05},],
+        "id": "godeok_tunnel",
+        "displayName": "[세종포천선] 고덕터널",
+        "locationName": "고덕터널",
+        "searchNames": ["[세종] 고덕터널", "고덕터널", "세종 2"],
+        "searchRegions": [{"lat": 37.5436, "lon": 127.1685, "delta": 0.05}],
     },
     {
         "id": "dallaenae_2",
